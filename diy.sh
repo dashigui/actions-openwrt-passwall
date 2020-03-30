2546d399d185996206bb45562132abd413b0eacc
dashigui <53265649+dashigui@users.noreply.github.com>
2020-03-30 11:29:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.250/g' package/base-files/files/bin/config_generate
+git clone https://github.com/vernesong/OpenClash.git
+./scripts/feeds update -a
+./scripts/feeds install -a
